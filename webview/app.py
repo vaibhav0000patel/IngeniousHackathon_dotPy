f668cf773fe8c233d95fcacf46771916979297a5
Vaibhav Patel <vaibhav0000patel@gmail.com>
2018-03-03 15:05:17 +0530

    created connection with postgresql

--- webview/app.py
+++ webview/app.py
@@ -7,5 +7,68 @@ def index():
     return render_template('google_maps.html')
 
 
+
+
+
+def set_userdata(data,sender_id):
+    conn = psycopg2.connect(host="localhost",database="fbsa", user="root", password="123")
+    cur = conn.cursor()
+    cur.execute("Select * from user_data where sender_id='"+str(sender_id)+"'")
+    userdata = cur.fetchall()
+    if userdata:
+        query_data = ""
+        for key in data:
+            query_data += key+"='"+str(data[key])+"',"
+        cur.execute("UPDATE user_data SET "+query_data[:-1]+" WHERE sender_id='"+str(sender_id)+"';")
+    else:
+        query_data = "("
+        for key in cols:
+            query_data += "'"+(str(data[key]) if key in data else None)+"',"
+        query_data = query_data[:-1]+")"
+        cur.execute("INSERT into user_data (sender_id,name,phone,email,status,lon,lat) VALUES "+query_data+";")
+    conn.commit()
+    cur.close()
+    return data
+
+def set_user_interests(data,sender_id):
+    conn = psycopg2.connect(host="localhost",database="fbsa", user="root", password="123")
+    cur = conn.cursor()
+    cur.execute("delete from user_interests where sender_id='"+str(sender_id)+"'")
+    for interest in data:
+        if interest!="" or interest!=None:
+            cur.execute("INSERT into user_interests (sender_id,interest) VALUES ('"+str(sender_id)+"','"+str(interest)+"');")
+    conn.commit()
+    cur.close()
+    return data
+
+def get_userdata(sender_id):
+    conn = psycopg2.connect(host="localhost",database="fbsa", user="root", password="123")
+    cur = conn.cursor()
+    cur.execute("Select * from user_data where sender_id='"+str(sender_id)+"'")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
+def get_user_interests(sender_id):
+    conn = psycopg2.connect(host="localhost",database="fbsa", user="root", password="123")
+    cur = conn.cursor()
+    cur.execute("Select * from user_interests where sender_id='"+str(sender_id)+"'")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
+def get_user_interest_data(interest):
+    conn = psycopg2.connect(host="localhost",database="fbsa", user="root", password="123")
+    cur = conn.cursor()
+    cur.execute("Select * from user_data ud,user_interests ui where ud.sender_id=ui.sender_id and ui.interest='"+interest+"';")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
+
+
 if __name__ == '__main__':
     app.run()
